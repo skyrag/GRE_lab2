51a0fea813498d4a583d64315d541a5bbc965962
Davtek11 <david.berger@heig-vd.ch>
2025-04-26 23:10:18 +0200

    algo v1

--- src/main/java/ch/heig/gre/groupD/AStar.java
+++ src/main/java/ch/heig/gre/groupD/AStar.java
@@ -1,11 +1,16 @@
 package ch.heig.gre.groupD;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 import ch.heig.gre.graph.GridGraph2D;
 import ch.heig.gre.graph.PositiveWeightFunction;
 import ch.heig.gre.graph.VertexLabelling;
 import ch.heig.gre.maze.GridMazeSolver;
-
-import java.util.*;
 
 public final class AStar implements GridMazeSolver {
   public enum Heuristic {
@@ -44,13 +49,16 @@ public final class AStar implements GridMazeSolver {
 
 
     //calcul de l'heuristique CAS DIJKSTRA
-    int[] heuristicTab = new int[grid.nbVertices()];
-    Arrays.fill(heuristicTab, 0); //devrait être null
-
+    double[] heuristicTab = new double[grid.nbVertices()];
+    Arrays.fill(heuristicTab, 0);
+    //coordonnées destination
+    int destX = destination % grid.width();
+    int destY = destination / grid.height();
+    int cMin = weights.minWeight();
 
     //Queue de priorité des sommets à traiter
     PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(
-            Comparator.comparingInt(v -> distance[v] + heuristicTab[v]));
+            Comparator.comparingDouble(v -> (double) distance[v] + heuristicTab[v]));
     priorityQueue.add(source);
     distance[source] = 0;
 
@@ -73,15 +81,45 @@ public final class AStar implements GridMazeSolver {
       }
 
       //Pour chaque successeur
-      for (Integer i : grid.neighbors(vertex)) {
-        if (distance[i] > distance[vertex] + weights.get(vertex, i)) {
-
-          distance[i] = distance[vertex] + weights.get(vertex, i);
-          pred[i] = vertex;
-
-          if (!processed.getLabel(i)) {
-            priorityQueue.add(i);
+      for (Integer successor : grid.neighbors(vertex)) {
+        if (distance[successor] > distance[vertex] + weights.get(vertex, successor)) {
+          
+          //calculer heuristic si distance = infini
+          if (distance[successor] == Integer.MAX_VALUE) {
+            //coordonnées actuel 
+              int vX = successor % grid.width();
+              int vY = successor / grid.height();
+              switch (heuristic) {
+                case DIJKSTRA -> {
+                  //valeur deja à 0
+                  break;
+                }
+                case INFINITY_NORM -> {
+                  heuristicTab[successor] = cMin * Math.max(Math.abs(destX - vX), Math.abs(destY - vY));
+                  break;
+                }
+                case EUCLIDEAN_NORM -> {
+                  heuristicTab[successor] = cMin * Math.floor(Math.hypot(destX - vX, destY - vY));
+                  break;
+                }
+                case MANHATTAN -> {
+                  heuristicTab[successor] = cMin * (Math.abs(destX - vX) + Math.abs(destY - vY));
+                  break;
+                }
+                case K_MANHATTAN -> {
+                  heuristicTab[successor] = kManhattan * cMin * (Math.abs(destX - vX) + Math.abs(destY - vY));
+                  break;
+                }
+                default -> {break;}
+              }
           }
+
+          distance[successor] = distance[vertex] + weights.get(vertex, successor);
+          pred[successor] = vertex;
+
+          //Mise a jour ou ajout dans la priority queue
+          priorityQueue.remove(successor); //remove seulement s'il est présent
+          priorityQueue.add(successor);
         }
       }
     }
